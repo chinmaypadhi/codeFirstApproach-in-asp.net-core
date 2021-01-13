bb843233bda4c040bbb8e5b62942e53de552055e
chinmay padhi <pahichinmay1998@gmail.com>
2021-01-13 08:54:35 +0530

    Add project files.

---
+++ Context/EmployeeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace codeFirstApproach_in_asp.net_core.Context
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        DbSet<Employee> Employees { get; set; }
+    }
+}
+
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace codeFirstApproach_in_asp.net_core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
